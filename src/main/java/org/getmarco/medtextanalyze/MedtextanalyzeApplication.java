73e3d84abac233f261c6902ec9fe37f1420800cf
Marco Chiesa <getmarcochiesa@gmail.com>
2019-10-07 22:19:10 -0500

    initial files

---
+++ src/main/java/org/getmarco/medtextanalyze/MedtextanalyzeApplication.java
@@ -0,0 +1,13 @@
+package org.getmarco.medtextanalyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedtextanalyzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedtextanalyzeApplication.class, args);
+	}
+
+}
